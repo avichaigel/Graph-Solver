814c438d1cd8ffba6c395886ff6d0e8db82a9a67
Ofir Netzer <ofir.netz@gmail.com>
2020-01-14 18:51:18 +0200

    Solver

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include "ClientHandler.h"
+#include "StringReverser.h"
 
 
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+//    StringReverser* test = new StringReverser();
     return 0;
 }
