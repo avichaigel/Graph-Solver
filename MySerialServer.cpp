ac5ed6eaa014cd7a71e60c415a9a838a627821bb
avichaigel <avichaig@gmail.com>
2020-01-19 17:39:43 +0200

    progress

--- MySerialServer.cpp
+++ MySerialServer.cpp
@@ -11,24 +11,24 @@
 
 
 void start(int port, ClientHandler* c) {
-    while (true) {
-        //create socket
-        int socketfd = socket(AF_INET, SOCK_STREAM, 0);
-        if (socketfd == -1) {
-            //error
-            cerr << "Could not create a socket" << endl;
-        }
-        //bind socket to IP address
-        // we first need to create the sockaddr obj.
-        sockaddr_in address; //in means IP4
-        address.sin_family = AF_INET;
-        address.sin_addr.s_addr = INADDR_ANY; //give me any IP allocated for my machine
-        address.sin_port = htons(port); //we need to convert our number to a number that the network understands
+    //create socket
+    int socketfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (socketfd == -1) {
+        //error
+        cerr << "Could not create a socket" << endl;
+    }
+    //bind socket to IP address
+    // we first need to create the sockaddr obj.x
+    sockaddr_in address; //in means IP4
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY; //give me any IP allocated for my machine
+    address.sin_port = htons(port); //we need to convert our number to a number that the network understands
 
-        //the actual bind command
-        if (bind(socketfd, (struct sockaddr *) &address, sizeof(address)) == -1) {
-            cerr << "Could not bind the socket to an IP" << endl;
-        }
+    //the actual bind command
+    if (bind(socketfd, (struct sockaddr *) &address, sizeof(address)) == -1) {
+        cerr << "Could not bind the socket to an IP" << endl;
+    }
+    while (true) {
 
         //making socket listen to the port
         if (listen(socketfd, 5) == -1) { //can also set to SOMAXCON (max connections)
@@ -63,8 +63,9 @@ void start(int port, ClientHandler* c) {
 
 void MySerialServer::open(int port, ClientHandler* c) {
     //call the thread that listens to the simulator and updates values
-    thread thread1(start, port, c);
-    thread1.detach();
+    start(port, c);
+    /*    thread thread1(start, port, c);
+    thread1.detach();*/
 }
 
 void MySerialServer::close() {
