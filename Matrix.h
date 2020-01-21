03072ac388296cb5a644138fbd16d9241229efdf
avichaigel <avichaig@gmail.com>
2020-01-21 20:36:35 +0200

    progress

--- Matrix.h
+++ Matrix.h
@@ -7,17 +7,29 @@
 
 #include "State.h"
 #include "Point.h"
+#include "ISearchable.h"
 #include <vector>
 
 using namespace std;
 
-class Matrix {
+class Matrix : public ISearchable<Point> {
+public:
+
 private:
     vector<vector<State<Point*>*>> outer;
+    State<Point>* initialState;
+    State<Point>* goalState;
 public:
     vector<vector<State<Point*>*>> &getOuter() {
         return outer;
     }
+    void setInitialState(State<Point> *initial);
+    void setGoalState(State<Point> *goal);
+    State<Point>*getInitialState() override;
+    bool isGoalState(State<Point> *s) override;
+    vector<State<Point> *> getAllPossibleStates(State<Point> *s) override;
+    Point getGoalState() override;
+    string to_string() override;
 };
 
 
